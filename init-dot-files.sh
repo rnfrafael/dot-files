11af2c442380d567ef1511a3f50b6c07faf2c996
Rafael Fontenele <rnf.rafael@gmail.com>
2023-08-14 13:19:14 -0300

    fix: fix links for git clone

--- init-dot-files.sh
+++ init-dot-files.sh
@@ -1,16 +1,18 @@
 #!bin/bash
 
 sudo apt update
-sudo apt install neovim -y
 sudo apt install git -y
+sudo apt install neovim -y
 sudo apt install yadm -y
 cd ~ 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 curl -fsSL https://fnm.vercel.app/install | bash # install FNM - Fast Node Manager
 #zsh Plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/zsh
-git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh
-git clone git clone https://github.com/zsh-users/zsh-history-substring-search ~/zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/zsh
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/zsh
 
 yadm git clone https://github.com/rnfrafael/dot-files.git ~/
+
+# wget -O - https://raw.githubusercontent.com//rnfrafael/dot-files/main/init-dot-files.sh | bash
