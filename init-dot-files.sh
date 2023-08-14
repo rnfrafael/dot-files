a9d749f6af24d3fe9024d5ec1fb06a9b6de4a062
Rafael Fontenele <rnf.rafael@gmail.com>
2023-08-14 15:47:07 -0300

    feat: add neovim over asdf and Plug for plugins

--- init-dot-files.sh
+++ init-dot-files.sh
@@ -2,10 +2,23 @@
 
 sudo apt update
 sudo apt install git -y
-sudo apt install neovim -y
 sudo apt install yadm -y
 sudo apt install zsh -y
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+source ~/.bashrc
+
+##NeoVim
+echo "[-] install NeoVim [-]"
+asdf plugin add neovim
+asdf install neovim 0.9.1
+echo "[-] install Plug for nvim[-]"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sudo apt install neovim -y
+#should run :PlugInstall inside NeoVim
+echo "[-] Done NeoVim [-]"
+
 cd ~ 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 curl -fsSL https://fnm.vercel.app/install | bash # install FNM - Fast Node Manager
@@ -14,6 +27,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosu
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.zsh/zsh-you-should-use
+
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
 
 echo "[-] Download fonts [-]"
 sudo apt install fontconfig -y
